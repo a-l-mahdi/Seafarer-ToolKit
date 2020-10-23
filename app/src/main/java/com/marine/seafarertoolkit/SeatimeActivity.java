380f67256fd1b8b9f4ec49db88da70a685d1c2d9
Hamid <almomahedoon.lel.mahdi@gmail.com>
2020-10-23 17:35:28 +0330

    update

--- app/src/main/java/com/marine/seafarertoolkit/SeatimeActivity.java
+++ app/src/main/java/com/marine/seafarertoolkit/SeatimeActivity.java
@@ -27,39 +27,39 @@ public class SeatimeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seatime);
 
-
-        sharedPrefImage = this.getSharedPreferences(profileImage, Context.MODE_PRIVATE);
-
-
-        profileImageUri = sharedPrefImage.getString(ImageUri, "");
-
-        Toast.makeText(this, profileImageUri + "", Toast.LENGTH_SHORT).show();
-
-        Intent intent = new Intent("com.intsig.camscanner.ACTION_SCAN");
-// Or content uri picked from gallery
-        Uri uri = Uri.fromFile(new File("/sdcard/source.jpg"));
-        intent.putExtra(Intent.EXTRA_STREAM, "file:///data/user/0/com.marine.seafarertoolkit/cache/1593540052316.jpg");
-        intent.putExtra("scanned_image", "file:///data/user/0/com.marine.seafarertoolkit/cache/150.jpg");
-        intent.putExtra("pdf_path", "/sdcard/processed.jpg");
-        intent.putExtra("org_image", "/sdcard/org.jpg");
-        startActivityForResult(intent, REQUEST_CODE);
-
-
-    }
-
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE) {
-//            int responseCode = data.getIntExtra("RESULT_OK", -1);
-            if (requestCode == Activity.RESULT_OK) {
-                Toast.makeText(this, "sucsses", Toast.LENGTH_SHORT).show();
-                // Success
-            } else if (resultCode == Activity.RESULT_FIRST_USER) {
-                // Fail
-            } else if (resultCode == Activity.RESULT_CANCELED) {
-                // User canceled
-            }
-        }
+//
+//        sharedPrefImage = this.getSharedPreferences(profileImage, Context.MODE_PRIVATE);
+//
+//
+//        profileImageUri = sharedPrefImage.getString(ImageUri, "");
+//
+//        Toast.makeText(this, profileImageUri + "", Toast.LENGTH_SHORT).show();
+//
+//        Intent intent = new Intent("com.intsig.camscanner.ACTION_SCAN");
+//// Or content uri picked from gallery
+//        Uri uri = Uri.fromFile(new File("/sdcard/source.jpg"));
+//        intent.putExtra(Intent.EXTRA_STREAM, "file:///data/user/0/com.marine.seafarertoolkit/cache/1593540052316.jpg");
+//        intent.putExtra("scanned_image", "file:///data/user/0/com.marine.seafarertoolkit/cache/150.jpg");
+//        intent.putExtra("pdf_path", "/sdcard/processed.jpg");
+//        intent.putExtra("org_image", "/sdcard/org.jpg");
+//        startActivityForResult(intent, REQUEST_CODE);
+//
+//
+//    }
+//
+//
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == REQUEST_CODE) {
+////            int responseCode = data.getIntExtra("RESULT_OK", -1);
+//            if (requestCode == Activity.RESULT_OK) {
+//                Toast.makeText(this, "sucsses", Toast.LENGTH_SHORT).show();
+//                // Success
+//            } else if (resultCode == Activity.RESULT_FIRST_USER) {
+//                // Fail
+//            } else if (resultCode == Activity.RESULT_CANCELED) {
+//                // User canceled
+//            }
+//        }
     }
 }
